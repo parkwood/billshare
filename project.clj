f0b1eed3568d5a8503f78b4159049a370b850933
parkwood <henrywidd@yahoo.com>
2011-12-11 10:50:17 +0000

    tidy up

--- project.clj
+++ project.clj
@@ -2,9 +2,7 @@
             :description "FIXME: write this!"
             :dependencies [[org.clojure/clojure "1.3.0"]
                            [noir "1.2.1"]
-                           [org.clojure/java.jdbc "0.0.6"]
-                           [mysql/mysql-connector-java "5.1.6"]
-                           [c3p0/c3p0 "0.9.1.2"]
-                           [appengine-magic "0.4.3"]]
-            :main billshare.server)
+                           ]
+            :dev-dependencies [[appengine-magic "0.4.6"]] 
+            )
 
--- src/billshare/core.clj
+++ src/billshare/core.clj
@@ -3,12 +3,11 @@
 	    [noir.util.gae :as noir]))
 
 ;; load all of the routes (defpage's) so they get def'd
-;(require 'billshare.views.common)
 (require 'billshare.views.google-tests)
 (require 'billshare.views.root)
 (require 'billshare.views.account-manager)
 (require 'billshare.views.groups-manager)
-;(require 'billshare.views.welcome)
+
 
 ;; def the appengine app
 (gae/def-appengine-app billshare-app (noir/gae-handler nil))

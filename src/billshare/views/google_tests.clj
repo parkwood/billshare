be0a755eada3e759a9522ed61370c3387c5af3ea
parkwood <henrywidd@yahoo.com>
2011-12-11 10:20:46 +0000

    sort out eclipse issue

--- src/billshare/views/google_tests.clj
+++ src/billshare/views/google_tests.clj
@@ -1,11 +1,10 @@
 (ns billshare.views.google-tests
    (:use [noir.core :only [defpage pre-route]]
-         [noir.response :only [redirect]]
-         ;[noir.session :as session]
-        [hiccup.core :only [html]]
-        [hiccup.page-helpers :only [link-to]]
-        [appengine-magic.services.user :as guser]
-        [billshare.models.user-service :as ds-user-service]))
+         [noir.response :only [redirect]]         
+         [hiccup.core :only [html]]
+         [hiccup.page-helpers :only [link-to]]
+         [appengine-magic.services.user :as guser]
+         [billshare.models.user-service :as ds-user-service]))
 
 (defpage "/user" []  
   (let [user (guser/current-user)
@@ -21,13 +20,9 @@
 (redirect (guser/login-url :destination "/user"))))
 
 (defpage "/login" []  
-  "
-   how do you become a user?
-   do you have to have a google accout?
-   do i bother having a user table or just assoc attrs with user"
+  ""
   (let [user (guser/current-user)] 
     (html [:h1 "Hello, " (if user (.getNickname user) "World") "!"]
           [:p (link-to (guser/login-url) "sign in")]
           [:p (link-to (guser/logout-url) "sign out")]))
   )
-
